0c28ec730af1eba92704f6760314cb7d9f1d12e7
Alejandro <alefer540@alu.centroafuera.es>
2021-11-22 14:05:31 +0100

    22/11/2021

--- app/src/main/java/com/example/entregalistener/MainActivity.kt
+++ app/src/main/java/com/example/entregalistener/MainActivity.kt
@@ -14,44 +14,46 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.b1.setOnClickListener {
 
-            binding.tv1.text= numeroAleatorio().toString()
+            binding.tv1.text = numeroAleatorio().toString()
+            binding.b4.visibility = View.GONE
         }
 
         binding.b2.setOnClickListener {
 
-            binding.tv1.text=letraAleatoria()
+            binding.tv1.text = letraAleatoria()
+            binding.b4.visibility = View.GONE
         }
-        var npalabra= ""
+        var npalabra = ""
         binding.b3.setOnClickListener {
-            npalabra=palabraAleatoria()
-            binding.tv1.text=npalabra
-        }
+            npalabra = palabraAleatoria()
+            binding.tv1.text = npalabra
+            binding.b4.visibility = View.VISIBLE
 
-        binding.tv1.doOnTextChanged { text, start, before, count ->
-            if (count == 5) {
-                binding.b4.visibility = View.VISIBLE
-            }else{
-                binding.b4.visibility=View.GONE
-            }
-        }
-        binding.b4.setOnClickListener{
-            var l1=npalabra.toCharArray()
-            var resultado=""
-            var posicion=(Random.nextInt(4))
-            var letraale= Random.nextInt(99, 122).toChar()
-            Toast.makeText(this,"cambio la letra ${l1[posicion]} por la letra aleatoria $letraale" +binding.tv1.text, Toast.LENGTH_LONG).show()
-            npalabra.replace(l1[posicion],letraale)
+
         }
 
 
+        binding.b4.setOnClickListener {
+            var cadena = binding.tv1.text
+            var tamanio = Random.nextInt(1, cadena.length)
+            var posletra=cadena[tamanio]
+            var letraaleatoria: Char = Random.nextInt(97, 122).toChar()
+            cadena= cadena.replaceRange(tamanio, tamanio + 1, letraaleatoria.toString())
+            binding.tv1.text = cadena
+            Toast.makeText(this,"nueva letra " +letraaleatoria+ " Posicion " + posletra,Toast.LENGTH_LONG).show()
+
+        }
 
 
     }
+
 
     fun numeroAleatorio(): Int {
         var numero = 0
@@ -72,5 +74,7 @@ class MainActivity : AppCompatActivity() {
         }
         return palabra
     }
-
 }
+
+
+
